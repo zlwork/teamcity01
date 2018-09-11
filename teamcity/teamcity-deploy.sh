e9cf01395598233562cdd37ffa9f7999b5b2326f
zlowk <zlwork@mail.ru>
2018-09-11 05:01:07 +0000

    ezugi office

--- teamcity/teamcity-deploy.sh
+++ teamcity/teamcity-deploy.sh
@@ -16,7 +16,7 @@ mkdir -p .ansible-playbook
 cat <<EOT >  .ansible-playbook/inventory-file.yml
 
 [$VARIABLE_HOST]
-$IP_HOST ansible_connection=ssh ansible_ssh_user=dev
+$IP_HOST ansible_connection=ssh ansible_ssh_user=dev ansible_port:2222
 
 EOT
 
